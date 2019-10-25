74ae04d64d4defb2c83e4461d7b4d6f80ddd4735
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-25 16:41:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,9 @@
 def find_element_index(array, value_to_find)
-  array = [1,2,3,4,5]
-
+  value_to_find[index]
 end
 
 def find_max_value(array)
-  array = [1,2,3,4,5]
-  
-  array.length.times { |index|
-  puts array [index]
+
 end
 
 def find_min_value(array)
